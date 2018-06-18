3c7099920511abc21c3b2f922fa7c66f45c2d51c
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-18 16:16:47 -0700

    remove

--- src/com/sreeharis/ota/Updater.java
+++ src/com/sreeharis/ota/Updater.java
@@ -1,15 +1,9 @@
 package com.sreeharis.ota;
 import android.widget.Toast;
-import android.support.multidex.MultiDex;
 public final class Updater extends android.app.Activity {
 	@Override
 	protected void onCreate(android.os.Bundle activityState) {
 		super.onCreate(activityState);
 		Toast.makeText(Updater.this, "Hello, World", Toast.LENGTH_LONG).show();
-	}
-	@Override
-	protected void attachBaseContext(android.content.Context base) {
-		super.attachBaseContext(base);
-		MultiDex.install(this);
 	}
 }

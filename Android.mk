bd8c383d61773c0299de5f9a036d91850fff6a0a
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-19 15:31:51 -0700

    Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := OTAUpdater
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PACKAGE)

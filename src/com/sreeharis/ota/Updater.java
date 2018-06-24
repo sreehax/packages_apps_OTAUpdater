d480263d8db56eabef2cb679972aeb5b1f64eed3
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-24 16:13:44 -0700

    '

--- src/com/sreeharis/ota/Updater.java
+++ src/com/sreeharis/ota/Updater.java
@@ -1,12 +1,12 @@
 package com.sreeharis.ota;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
-public final class Updater extends AppCompatActivity {
+public final class Updater extends Activity {
 	private Toolbar toolbar;
 	@Override
 	protected void onCreate(Bundle activityState) {
@@ -20,5 +20,7 @@ public final class Updater extends AppCompatActivity {
 				pm = getPackageManager();
 				Intent LaunchIntent = pm.getLaunchIntentForPackage("com.android.settings");
 				startActivity(LaunchIntent);
+			}
+		}
 	}
 }

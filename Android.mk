ae317b750b1b3ab10ad78c376782ca0aa3a21144
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-24 14:38:04 -0700

    AppCompat

--- Android.mk
+++ Android.mk
@@ -4,9 +4,12 @@ include $(CLEAR_VARS)
 LOCAL_USE_AAPT2 := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := OTAUpdater
 LOCAL_CERTIFICATE := platform
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
--- src/com/sreeharis/ota/Updater.java
+++ src/com/sreeharis/ota/Updater.java
@@ -1,7 +1,8 @@
 package com.sreeharis.ota;
 import android.os.Bundle;
-import android.app.Activity;
-public final class Updater extends Activity {
+import android.support.v7.app.AppCompatActivity;
+
+public final class Updater extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle activityState) {
 		super.onCreate(activityState);

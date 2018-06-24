2952ca9da5055b2cdaccae3db1d50fa0d58807cb
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-24 13:12:42 -0700

    Layout

--- src/com/sreeharis/ota/Updater.java
+++ src/com/sreeharis/ota/Updater.java
@@ -1,9 +1,12 @@
 package com.sreeharis.ota;
-import android.widget.Toast;
-public final class Updater extends android.app.Activity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class Updater extends AppCompatActivity {
 	@Override
-	protected void onCreate(android.os.Bundle activityState) {
+	protected void onCreate(Bundle activityState) {
 		super.onCreate(activityState);
-		Toast.makeText(Updater.this, "Hello, World", Toast.LENGTH_LONG).show();
+		setContentView(R.layout.main);
+
 	}
 }

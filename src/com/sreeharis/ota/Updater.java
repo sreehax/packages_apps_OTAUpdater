cff6bbdce4f62dae68708409f51e9d74ee322da9
Sreehari S <sreeharisreedev1@gmail.com>
2018-06-24 15:20:23 -0700

    back button

--- src/com/sreeharis/ota/Updater.java
+++ src/com/sreeharis/ota/Updater.java
@@ -1,12 +1,24 @@
 package com.sreeharis.ota;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 public final class Updater extends AppCompatActivity {
+	private Toolbar toolbar;
 	@Override
 	protected void onCreate(Bundle activityState) {
 		super.onCreate(activityState);
 		setContentView(R.layout.main);
-
+		toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PackageManager pm;
+				pm = getPackageManager();
+				Intent LaunchIntent = pm.getLaunchIntentForPackage("com.android.settings");
+				startActivity(LaunchIntent);
 	}
 }
